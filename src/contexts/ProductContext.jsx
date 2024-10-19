689c84a28847ad4d8a1846172c78ccbbe934fff3
JonathanArtigaDimas <dimas.jonathan.artiga@gmail.com>
2024-10-19 00:28:49 -0600

    cambio en el json y cambio de una api a usar el json local

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,4 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
+import productsData from '../JsonProducts.json';
 
 
 export const ProductContext = createContext();
@@ -7,6 +8,8 @@ const ProducProvider = ({children}) => {
   //Producto State
   const [products, setProducts] = useState([]);
   //Fetch de productos
+
+  /* API de fake store API
   useEffect(()=>{
       const fetchProducts = async ()=>{
           const response = await fetch('https://fakestoreapi.com/products');
@@ -14,7 +17,14 @@ const ProducProvider = ({children}) => {
           setProducts(data);
       };
       fetchProducts();
-  }, [])
+  }, []) */
+
+  useEffect(() => {
+    const loadProducts = () => {
+      setProducts(productsData); // Asigna directamente los datos del JSON local
+    };
+    loadProducts();
+  }, []);
 
 
   return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>;
